c36121ca26520353da60015e28b89b115a133734
Daniel Popa <danieleduardpopa@gmail.com>
2023-04-20 16:06:16 +0300

    show screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const ShowScreen = ({ navigation }) => {
@@ -12,13 +13,42 @@ const ShowScreen = ({ navigation }) => {
     
     return (
         <View>
-            <Text>{blogPost.title}</Text>
+            <Text style={styles.titleStyle}>{blogPost.title.toUpperCase()}:</Text>
+            <Text style={styles.contentStyle}>{blogPost.content}</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity 
+                onPress={() => navigation.navigate(
+                    'Edit', { id: navigation.getParam('id') }
+                )}
+            >
+                <MaterialIcons name='edit' size={30} style={{ marginRight: 10 }} />
+            </TouchableOpacity>
+        )
+    };
+};
 
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 20,
+        marginLeft: 5,
+        marginBottom: 15,
+        marginTop: 10
+        
+    },
+    contentStyle: {
+        fontSize: 16,
+        marginHorizontal: 5,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: 'black'
+    }
 });
 
 export default ShowScreen;
